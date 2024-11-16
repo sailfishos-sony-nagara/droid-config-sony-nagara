8ad369c1b86f9c85bf684f76dd5510bdecf6119e
Rinigus <rinigus.git@gmail.com>
2024-11-16 18:41:41 +0200

    Move overlay links from sparse/system

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 mount -o bind /dev/null /system/system_ext/etc/gsi/init.vndk-nodef.rc
+
+# mount used overlays
+WDIR=/overlays/.work
+
+mkdir -p $WDIR/system/lib
+mkdir -p $WDIR/system/lib64
+
+mount -t overlay overlay -o lowerdir=/system/lib,upperdir=/overlays/system/lib,workdir=$WDIR/system/lib /system/lib
+mount -t overlay overlay -o lowerdir=/system/lib64,upperdir=/overlays/system/lib64,workdir=$WDIR/system/lib64 /system/lib64
