7828e6328b7d59f6acdbda6d3631d7642c2c9df0
Rinigus <rinigus.git@gmail.com>
2024-11-16 20:53:45 +0200

    Handle overlays in a separate script

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
+# bind mounts
 mount -o bind /dev/null /system/system_ext/etc/gsi/init.vndk-nodef.rc
 
-# mount used overlays
-WDIR=/overlays/.work
-
-mkdir -p $WDIR/system/lib
-mkdir -p $WDIR/system/lib64
-
-mount -t overlay overlay -o lowerdir=/system/lib,upperdir=/overlays/system/lib,workdir=$WDIR/system/lib /system/lib
-mount -t overlay overlay -o lowerdir=/system/lib64,upperdir=/overlays/system/lib64,workdir=$WDIR/system/lib64 /system/lib64
+# overlays
+mount-overlays /overlays
