5d8f395407d78fbb6b8e5c2605e7f2dce5e561fb
Vlad G <b100dian@gmail.com>
2024-11-17 23:10:53 +0000

    overlay of links become files before graphical target

--- sparse/usr/bin/droid/droid-misc.sh
+++ sparse/usr/bin/droid/droid-misc.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 #Misc setup script
 
-ln -sf /dev/binderfs/* /dev/
+WHICH=/apex/com.android.art/lib/libnativehelper.so
+
+# wait for a file we know its last mounted in linkerconfig
+echo "Waiting for $WHICH to be mounted.."
+while [ ! -f $WHICH ]; do sleep 1; done;
+echo "Waiting for $WHICH done."
+
+# Transform softlinks into real copies where needed
+find /overlays -type l | while read SOFTLINK; do cp -f `readlink $SOFTLINK` $SOFTLINK; done
+
