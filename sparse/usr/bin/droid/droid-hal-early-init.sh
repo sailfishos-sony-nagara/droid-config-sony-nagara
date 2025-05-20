58c0404da1cbab63a39ee478d8f9f3b4ac38251b
Rinigus <rinigus.git@gmail.com>
2025-05-20 22:26:56 +0300

    Update init scripts

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # bind mounts to mask odm inits
-for init_s in init.sony-device-common.rc init.sony-platform.rc init.sony.rc ;
+for init_s in init.sony-platform.rc init.sony.rc ;
 do
    init=/odm/etc/init/$init_s
    echo Masking $init
