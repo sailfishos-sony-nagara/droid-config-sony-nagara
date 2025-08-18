e3d82f19e7d0323fc925d3a46568d436119a2393
Vlad G <b100dian@gmail.com>
2025-08-18 21:02:26 +0000

    Double tap to wake

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Droid HAL Early init: nothing to do"
+# prerequisite for double-tap-to wake
+echo 1 > /sys/devices/dsi_panel_driver/pre_sod_mode
+
+# accesible binder devices in /dev
+ln -sf /dev/binderfs/* /dev/
+
